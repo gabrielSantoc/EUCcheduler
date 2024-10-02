4858edb8ac4d12155a3957876cd55612e669cdf5
bakituhawsayongsayawbakitikaw <gabrielsantoc05@gmail.com>
2024-10-02 19:30:16 +0800

    add regster and login

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:my_schedule/auth/auth.dart';
 import 'package:my_schedule/auth/login.dart';
 import 'package:my_schedule/auth/register.dart';
-import 'package:my_schedule/shared/constants.dart';
+
 
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-
-  await dotenv.load(fileName: '.env');
   
   await Supabase.initialize(
-    url: "https://gktxmfxkiqixazprhzwq.supabase.co",
-    anonKey: api_key.toString(),
+    url: "https://vuphmshlifryuczfphoz.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1cGhtc2hsaWZyeXVjemZwaG96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc2NTUzNDcsImV4cCI6MjA0MzIzMTM0N30.jcGi4KNAo5KNZCrT2wdbNOPy3WcCG6uWRzjVlFZ0RpA",
   );
 
   runApp(const MyApp());
 }
+
+final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  LoginScreen()
+      home:  AuthScreen()
     );
   }
 }
