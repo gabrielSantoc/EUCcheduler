7ae4720a21258d85d9333c4400288d763a9d4975
bakituhawsayongsayawbakitikaw <gabrielsantoc05@gmail.com>
2024-10-02 12:10:16 +0800

    update login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:my_schedule/screens/login.dart';
-import 'package:my_schedule/screens/student_screen2.dart';
-import 'package:my_schedule/screens/testScreen.dart';
+import 'package:my_schedule/auth/login.dart';
+import 'package:my_schedule/auth/register.dart';
 import 'package:my_schedule/shared/constants.dart';
 
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -14,8 +13,8 @@ void main() async{
   await dotenv.load(fileName: '.env');
   
   await Supabase.initialize(
-    url: "***REMOVED***",
-    anonKey: "***REMOVED***",
+    url: "https://gktxmfxkiqixazprhzwq.supabase.co",
+    anonKey: api_key.toString(),
   );
 
   runApp(const MyApp());
@@ -28,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  ScheduleScreen()
+      home:  LoginScreen()
     );
   }
 }
